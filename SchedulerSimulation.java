41cfc5a4788e8bf952affa30fe3bfe7dcd8db2ee
445052513-jpg <445052513@std.psau.edu.sa>
2026-03-28 08:06:47 +0300

    Feature 2: Implemented context switches tracking mechanism

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -149,6 +149,7 @@ class Process implements Runnable {
 }
 
 public class SchedulerSimulation {
+    static int contextSwitchCount = 0;//Feature 2: Counter for context switches
     public static void main(String[] args) {
         // ⚠️ IMPORTANT: Put your student ID here to seed the random number generator
         // This makes your output unique to you - DO NOT forget to change this!
@@ -224,7 +225,8 @@ public class SchedulerSimulation {
         while (!processQueue.isEmpty()) {
             // Get the next thread from the queue (FIFO)
             Thread currentThread = processQueue.poll(); // Dequeues the next thread
-            
+            contextSwitchCount++;//Featuer 2:Increment context switch count
+
             // Print the current process queue (list of process IDs in the queue)
             System.out.println(Colors.BOLD + Colors.MAGENTA + "┌─ Ready Queue " + "─".repeat(65) + Colors.RESET);
             System.out.print(Colors.MAGENTA + "│ " + Colors.RESET + Colors.BRIGHT_WHITE + "[" + Colors.RESET);
@@ -281,6 +283,7 @@ public class SchedulerSimulation {
         System.out.println(Colors.BOLD + Colors.BRIGHT_GREEN + 
                           "╚════════════════════════════════════════════════════════════════════════════════╝" + 
                           Colors.RESET + "\n");
+       System.out.println("Total context switches:"+ contextSwitchCount);//Feature 2: print total context switches
     }
     
     // Method to add a process to the queue and map, while printing a "ready" message
@@ -300,5 +303,6 @@ public class SchedulerSimulation {
                           Colors.RESET + Colors.BLUE + " added to ready queue" + Colors.RESET + 
                           " │ Burst time: " + Colors.YELLOW + process.getBurstTime() + "ms" + 
                           Colors.RESET +"|priority:"+Colors.BRIGHT_YELLOW +Process.getpriority());
+
     }
 }
